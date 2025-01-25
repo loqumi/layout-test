ded25441f74c204fe6498a1861362518437fc204
loqumi <loqumihh@gmail.com>
2025-01-25 20:14:40 +0100

    fix: deleted unused icons, replace icons to svg

--- src/Components/IdentificationTab.js
+++ src/Components/IdentificationTab.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import IconCalendar from '../icons/CalendarBlank.svg'
 
 const IdentificationTab = () => {
 	return (
@@ -31,7 +30,21 @@ const IdentificationTab = () => {
 						<td className="info-section">
 							<p>Транспортные средства</p>
 							<div className="additional-info">
-								<p className='calendar'><img src={IconCalendar} alt={'calendar'}/>2021</p>
+								<p className='calendar'>
+									<svg width="10" height="10" viewBox="0 0 10 10" fill="none"
+										 xmlns="http://www.w3.org/2000/svg">
+										<path
+											d="M8.125 1.5625H1.875C1.70241 1.5625 1.5625 1.70241 1.5625 1.875V8.125C1.5625 8.29759 1.70241 8.4375 1.875 8.4375H8.125C8.29759 8.4375 8.4375 8.29759 8.4375 8.125V1.875C8.4375 1.70241 8.29759 1.5625 8.125 1.5625Z"
+											stroke="#87878C" strokeLinecap="round" strokeLinejoin="round"/>
+										<path d="M6.875 0.9375V2.1875" stroke="#87878C" strokeLinecap="round"
+											  strokeLinejoin="round"/>
+										<path d="M3.125 0.9375V2.1875" stroke="#87878C" strokeLinecap="round"
+											  strokeLinejoin="round"/>
+										<path d="M1.5625 3.4375H8.4375" stroke="#87878C" strokeLinecap="round"
+											  strokeLinejoin="round"/>
+									</svg>
+									2021
+								</p>
 								<p>ДРУГАЯ МАРКА TOYOTA CROWN ГРЗ А111НХ125</p>
 								<p className='grey-text'>СТС 235234634673476</p>
 							</div>
@@ -70,7 +83,21 @@ const IdentificationTab = () => {
 						<td className="info-section">
 							<p>Транспортные средства</p>
 							<div className="additional-info">
-								<p className='calendar'><img src={IconCalendar} alt={'calendar'}/>2021</p>
+								<p className='calendar'>
+									<svg width="10" height="10" viewBox="0 0 10 10" fill="none"
+										 xmlns="http://www.w3.org/2000/svg">
+										<path
+											d="M8.125 1.5625H1.875C1.70241 1.5625 1.5625 1.70241 1.5625 1.875V8.125C1.5625 8.29759 1.70241 8.4375 1.875 8.4375H8.125C8.29759 8.4375 8.4375 8.29759 8.4375 8.125V1.875C8.4375 1.70241 8.29759 1.5625 8.125 1.5625Z"
+											stroke="#87878C" strokeLinecap="round" strokeLinejoin="round"/>
+										<path d="M6.875 0.9375V2.1875" stroke="#87878C" strokeLinecap="round"
+											  strokeLinejoin="round"/>
+										<path d="M3.125 0.9375V2.1875" stroke="#87878C" strokeLinecap="round"
+											  strokeLinejoin="round"/>
+										<path d="M1.5625 3.4375H8.4375" stroke="#87878C" strokeLinecap="round"
+											  strokeLinejoin="round"/>
+									</svg>
+									2021
+								</p>
 								<p>ДРУГАЯ МАРКА TOYOTA CROWN ГРЗ А111НХ125</p>
 								<p className='grey-text'>СТС 235234634673476</p>
 							</div>
