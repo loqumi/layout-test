50a1aa7c837f126f4d565ec21cd0d827accdf033
loqumi <loqumihh@gmail.com>
2025-01-25 19:50:56 +0100

    feat: added animations to header links, removed icon

--- src/Components/ButtonGoBack.js
+++ src/Components/ButtonGoBack.js
@@ -1,10 +1,12 @@
 import React from "react";
-import IconCaretDown from "../icons/CaretDown.svg";
 
 const LinkGoBack = ({isVisible, setIsSidebarVisible}) => {
 	return (
 		<a onClick={() => setIsSidebarVisible(!isVisible)} className="back-link">
-			<img alt={'pdf icon'} src={IconCaretDown} />
+			<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+				<path className="icon" d="M10 13L5 8L10 3" stroke="#E7682A" strokeLinecap="round" strokeLinejoin="round"/>
+			</svg>
+
 			Вернуться к отчетам
 		</a>
 	)
